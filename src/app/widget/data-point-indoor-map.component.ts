aaad874a05f9495bb6bb074b7173e6b0fb638a28
dikr-c8y <divya.krishnamurthy@cumulocity.com>
2025-11-04 19:09:49 +0530

    fix maxzoom

--- src/app/widget/data-point-indoor-map.component.ts
+++ src/app/widget/data-point-indoor-map.component.ts
@@ -350,6 +350,7 @@ export class DataPointIndoorMapComponent
 
     this.leaf
       .tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       })
@@ -544,6 +545,7 @@ export class DataPointIndoorMapComponent
     });
     this.leaf
       .tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       })
